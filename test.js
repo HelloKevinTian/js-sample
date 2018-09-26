64bc5a922b0bf25be1fd41eff5e20e6bf1544d87
tianwen <tianwen@fotoable.com>
2018-09-26 14:38:12 +0800

    测试async function

--- test.js
+++ test.js
@@ -1,27 +1,22 @@
-function getLastMonth(n) {
-    const nowDate = new Date();
-    const nowYear = nowDate.getFullYear();
-    const nowMonth = nowDate.getMonth() + 1; //0~11 +1 1~12
-    if (nowMonth > n) {
-        let m = nowMonth - n;
-        m = ('00' + m).substr(('' + m).length);
-        return nowYear + m;
-    } else {
-        let y = nowYear - 1;
-        let m = 12 - (n - nowMonth);
-        m = ('00' + m).substr(('' + m).length);
-        return y + m;
-    }
+const EventEmitter = require('events').EventEmitter;
+
+const emitter = new EventEmitter();
+
+const fun = function() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('dida...')
+            resolve(1);
+        }, 2000);
+    });
 }
 
-console.log(getLastMonth(1));
-console.log(getLastMonth(2));
-console.log(getLastMonth(3));
-console.log(getLastMonth(4));
-console.log(getLastMonth(5));
-console.log(getLastMonth(6));
-console.log(getLastMonth(7));
-console.log(getLastMonth(8));
-console.log(getLastMonth(9));
-console.log(getLastMonth(10));
-console.log(getLastMonth(11));
+emitter.on('test', async function(params) {
+    
+    console.log('1111', params);
+    const x = await fun();
+    console.log('2222', params, x);
+
+});
+
+emitter.emit('test', [123]);
