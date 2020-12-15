1ac903c991a3c2bee935ed3abc96e00dc5af4d30
tianwen <tianwen@fotoable.com>
2020-12-15 11:28:08 +0800

    [feat] 测试asyncFunction并发

--- test.js
+++ test.js
@@ -1,37 +1,46 @@
-class CustomAPI {
-    getAdName(a, b) {
-        console.log(`a[${a}] b[${b}]`);
-    }
+/**
+ * 测试asyncFunction并发执行
+ */
+
+const delay = function(t = 2000) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, t);
+    })
 }
 
-class FBAPI {
-    constructor() {
-        this.customFuncMap = new Map();
-    }
-
-    addCustomFunc(funcName, func) {
-        if (this.customFuncMap.has(funcName)) {
-            throw new Error('FBAdAPI function exists');
-        } else {
-            this.customFuncMap.set(funcName, func);
-        }
-    }
-
-    execCustomFunc(funcName, ...args) {
-        console.log(this.customFuncMap)
-        if (this.customFuncMap.has(funcName)) {
-            const execFunc = this.customFuncMap.get(funcName);
-            execFunc(...args);
-        } else {
-            throw new Error('FBAdAPI function not exist');
-        }
-    }
+const func1 = async function() {
+    console.log('func1-start')
+    let arr = [];
+    await delay(5000);
+    console.log('func1-end')
+    arr.push(1);
+    return arr;
+}
+const func2 = async function() {
+    console.log('func2-start')
+    let arr = [];
+    await delay(5000);
+    console.log('func2-end')
+    arr.push(2);
+    return arr;
+}
+const func3 = async function() {
+    console.log('func3-start')
+    let arr = [];
+    await delay(5000);
+    console.log('func3-end')
+    arr.push(3);
+    return arr;
 }
 
-const cusAPI = new CustomAPI();
-
-const API = new FBAPI();
-
-API.addCustomFunc('ab', cusAPI.getAdName.bind(cusAPI));
-
-API.execCustomFunc('ab', 111, 222, 333);
+setTimeout(async() => {
+    const a = [];
+    a.push(func1);
+    a.push(func2);
+    a.push(func3);
+    console.log('------start')
+    const res = await Promise.all(a.map(f => f()));
+    console.log(res);
+}, 100);
